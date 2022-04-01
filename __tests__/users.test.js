45035c74e9e5b6072b6d26ea0f36df09e583199e
adria <littleivanitsky@gmail.com>
2022-04-01 14:55:35 -0700

    created users table, started writing first test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,6 +3,14 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const agent = request.agent(app);
+jest.mock('../lib/utils/github');
+
+const mockUser = {
+  email: 'mock@user.com',
+  username: 'adria',
+  avatar: 'text',
+};
 describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +18,9 @@ describe('gitty routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  test('creates a new user', async () => {
+    const user = await request(app).post('/api/v1/users').send();
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    email TEXT,
+    username TEXT NOT NULL PRIMARY KEY,
+    avatar TEXT,
+);
